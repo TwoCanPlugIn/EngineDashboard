4938fc542c78ccccdc349c4977057e53978965a6
twocanplugin <twocanplugin@hotmail.com>
2020-12-08 22:23:15 +1100

    V1.3 Beta - fix Mac OSX build

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
+
 #
 # Build the  MacOS artifacts
-#
-
-# Fix broken ruby on the CircleCI image:
-if [ -n "$CI" ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
 
 set -xe
 
-set -o pipefail
+#
 # Check if the cache is with us. If not, re-install brew.
 brew list --versions libexif || brew update-reset
 
@@ -20,27 +15,24 @@ for pkg in cairo cmake gettext libarchive libexif python wget; do
     brew link --overwrite $pkg || brew install $pkg
 done
 
-brew list python@2 2>&1 >/dev/null && brew unlink python@2
-brew reinstall python3
-
 wget -q http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
 tar xJf wx312_opencpn50_macos109.tar.xz -C /tmp
-export PATH="/usr/local/opt/gettext/bin:$PATH"
-echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
+
+export MACOSX_DEPLOYMENT_TARGET=10.9
 
 rm -rf build && mkdir build && cd build
 cmake \
+  -DCMAKE_BUILD_TYPE=Release \
   -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312_opencpn50_macos109/bin/wx-config \
-  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312_opencpn50_macos109" \
-  -DCMAKE_INSTALL_PREFIX= \
+  -DCMAKE_INSTALL_PREFIX="/" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
-  "/" \
   ..
-make -sj2
-make package
+make -j $(sysctl -n hw.physicalcpu) VERBOSE=1 tarball
 
 wget -q http://opencpn.navnux.org/build_deps/Packages.dmg
 hdiutil attach Packages.dmg
 sudo installer -pkg "/Volumes/Packages 1.2.5/Install Packages.pkg" -target "/"
 make create-pkg
 
+# Install cloudsmith needed by upload script
+pip3 install cloudsmith-cli
